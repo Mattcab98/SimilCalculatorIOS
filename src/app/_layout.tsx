ad6164855cc61a894e5fe7f273543b8e67ee4e38
Matias Alvarez <matiasalvarezbustos@gmail.com>
2026-08-17 20:56:05 -0300

    proyect calculator init

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { View, Text, } from 'react-native'
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { Slot } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-SplashScreen.preventAutoHideAsync();
+// Imports FUENTES
+import { useFonts } from 'expo-font'
+// Imports Styles
+import { globalStyles } from '../../styles/global-styles'
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
+  
+  const [loaded] = useFonts ({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf')
+  })
+
+  if ( !loaded ) {
+    return null;
+  }
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+
+    <View style={globalStyles.background}>
+      <Slot />
+
+      <StatusBar style='light'/>
+    </View>
+
+  )
 }
+
+export default RootLayout
---
+++ styles/global-styles.ts
@@ -0,0 +1,13 @@
+import { Colors } from "@/constants/Colors"
+import { StyleSheet } from "react-native"
+
+export const globalStyles = StyleSheet.create ({
+    background: {
+        flex: 1,
+        backgroundColor: Colors.background
+    },
+
+    calculatorContainer: {
+
+    }
+})
